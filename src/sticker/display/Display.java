5ef41da9e12026c4b4f3d5fb67da38dd73d6c4ac
Sticker047 <gagileww@mail.ru>
2021-12-13 05:29:10 +0500

    Step 2

--- src/sticker/display/Display.java
+++ src/sticker/display/Display.java
@@ -2,29 +2,35 @@ package sticker.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public class Display {
     public static boolean created = false;
     public static JFrame window;
     public static Canvas content;
 
-    public static void create(int width, int height, String title) {
+    public static BufferedImage buffer;
+    public static int[] bufferData;
+    public static Graphics bufferGraphics;
+    public static int clearColor;
+
+    //temp
+    private static float delta = 0;
+    //end temp
+
+
+    public static void create(int width, int height, String title, int _clearColor) {
         if (created) return;
 
         window = new JFrame(title);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        content = new Canvas() {
-
-            public void paint(Graphics g) {
-                super.paint(g);
-                render(g);
-            }
-
-        };
+        content = new Canvas();
 
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
-        content.setBackground(Color.BLACK);
 
         window.setResizable(false);
         window.getContentPane().add(content);
@@ -32,16 +38,27 @@ public class Display {
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
 
+        created = true;
+    }
+
+    public static void clear() {
+        Arrays.fill(bufferData, clearColor);
     }
 
     public static void render() {
-        content.repaint();
+        bufferGraphics.setColor(new Color(0xff0000ff));
+        bufferGraphics.fillOval((int)(350 + Math.sin(delta) * 200), 250, 100, 100);
+        delta += 0.02f;
     }
 
-    public static void render(Graphics g) {
-        g.setColor(Color.white);
-        g.fillOval(400 - 50, 300 - 50, 100, 100);
+    public static void swapBuffers() {
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer, 0, 0, null);
     }
 
 }
